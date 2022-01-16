74b555861fa5d1b04ee2e6f373485cd61653f7b8
Sean Gillespie <seene3@gmail.com>
2022-01-16 18:32:16 -0500

    got notes to go back to right section

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -15,17 +15,12 @@ function createNewNote(body, notesArray) {
 
 router.get("/notes", (req, res) => {
   let results = notes;
-  //   if (req.query) {
-  //     results = filterByQuery(req.query, results);
-  //   }
+
   res.json(results);
 });
 
 router.post("/notes", (req, res) => {
-  // set id based on what the next index of the array will be
-  // req.body = notes.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
+  req.body.id = notes.length.toString();
 
   const note = createNewNote(req.body, notes);
   res.json(note);
